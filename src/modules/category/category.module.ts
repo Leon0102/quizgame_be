74b841c77b9c2bb1158eb24e14fb150e156061dd
Linh Nguyen <linh.hoang.nguyen@enlabsoftware.com>
2022-12-03 16:57:48 +0700

    fix: fix repository & new database

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryController } from './category.controller';
 import { Category } from './category.entity';
+import { CategoryRepository } from './category.repository';
 import { CategoryService } from './category.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [CategoryService, CategoryRepository],
   exports: [CategoryService],
 })
 export class CategoryModule {}
--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from '../category/category.module';
+import { QuizController } from './quiz.controller';
 import { Quiz } from './quiz.entity';
+import { QuizRepository } from './quiz.repository';
 import { QuizService } from './quiz.service';
 
 @Module({
-  controllers: [],
+  controllers: [QuizController],
   imports: [TypeOrmModule.forFeature([Quiz]), CategoryModule],
-  providers: [QuizService],
+  providers: [QuizService, QuizRepository],
 })
 export class QuizModule {}
