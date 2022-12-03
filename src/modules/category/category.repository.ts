a41efb609cea5053d81ccd79f9d6c048851132a5
Hoa Nguyen <hoanguyendn61@gmail.com>
2022-12-03 23:14:31 +0700

    feature: added a lib from nexus repo

--- src/modules/category/category.repository.ts
+++ src/modules/category/category.repository.ts
@@ -1,6 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { BaseRepository } from '../../utils/base.abstract';
+import { BaseRepository } from 'quizgame_lib';
 import { Category } from './category.entity';
 
 export class CategoryRepository extends BaseRepository<Category> {
